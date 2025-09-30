cccb914779658f26535490debbd1fe9b62d90182
rahman-04 <rahman395276@gmail.com>
2025-09-30 16:30:37 +0530

    Scanner Class

---
+++ UserInput.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println(c);
+        sc.close();
+    }
+}
