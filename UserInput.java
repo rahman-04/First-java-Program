495f4eff5734757306250efcca09140cee2e4e42
rahman-04 <rahman395276@gmail.com>
2025-09-30 16:41:53 +0530

    Update UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers :");
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = a + b;
