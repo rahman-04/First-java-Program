22e8bd27f69823641211a39bd859de95240d3c87
rahman-04 <rahman395276@gmail.com>
2025-10-01 20:13:06 +0530

    Basic For Loop

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hallo java");//printing hallo java 5 times
+        }
+        for (int i = 0; i < 5; i++) {
+           System.out.println(i);//printing 0 to 4 no.
+        }
+    }
+}
